6635b43d052436e26386bd42b74004c0c444c97d
SRP-mango <siriphicha.mango02@gmail.com>
2023-09-12 15:37:53 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <h1>twtter clone here</h1>
+
+      {/* sidebar
+        - navbar
+            - หน้าแรก
+            - สำรวจ
+            - การแจ้งเตือน
+            - ข้อความ
+            - รายชื่อ
+            - บุ๊คมาร์ก
+            - ยืนยันแล้ว
+            - ข้อมูลส่วนตัว
+            - เพิ่มเติม
+        - post button
+        - user account */}
+
+      {/* feed
+        - for you
+        - following
+        - post
+        - feed */}
+
+      {/* widget
+        -searchbar
+        -treading */}
+
     </div>
   );
 }
